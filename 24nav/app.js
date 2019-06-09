4d903784ac3529a41775414dfbec377c6d412138
qzuser <1522502267@qq.com>
2019-06-09 14:15:15 +0800

    nav

---
+++ 24nav/app.js
@@ -0,0 +1,35 @@
+  var oNav = document.getElementById('main');
+  var changePoint = oNav.offsetTop;
+  var oLogo = document.getElementById('logo');
+
+  function adjustNav(){
+    if(window.scrollY >= changePoint){
+        oNav.className = 'fixed-nav';
+        oLogo.style['max-width'] = '200px';
+
+    }else{
+        oNav.className = '';
+        oLogo.style['max-width'] = '0';
+    }
+  }
+  //把调整函数绑定至页面滚动事件
+  window.addEventListener('scroll',adjustNav);
+/*
+var oNav = document.querySelector("#main");
+var changePoint = oNav.offsetTop;
+var logo = document.getElementById("logo");
+function adjustNav(){
+  if(window.scrollY >= changePoint){
+    oNav.className = 'fixed-nav';
+    oLogo.style.max-width = "200px";
+  }
+  else{
+    oNav.className = '';
+    oLogo.style.max-width = '0';
+  }
+}
+window.addEventListener('scroll', adjustNav);
+*/
+
+
+
